ce8f537d8b0890dd521040b181472b8ab5d7b625
nanimox <nanimox@proton.me>
2025-07-31 11:18:37 -0300

    added threads to load persons while user gives input

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,11 +3,12 @@ mod crypto;
 mod db;
 mod user;
 
-use user::person::Person;
+use std::{thread, time::Duration};
 
 use crate::crypto::rsa::RSA;
+use user::person::Person;
 
-fn main() {
+fn run() {
     tauri::Builder::default()
         .setup(|app| {
             if cfg!(debug_assertions) {
@@ -19,14 +20,30 @@ fn main() {
             }
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![cycle])
+        .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-    #[tauri::command]
-    fn cycle(msg: String) -> String{
-        let mut person1 = Person::new(String::from("person1"));
-        let person2 = Person::new(String::from("person2 "));
-        RSA::encrypt(&mut person1, msg, &person2);
-        return RSA::decrypt(&person2)[0].clone();
+}
+
+fn main() {
+    let user_input_thread = thread::spawn(|| -> String {
+        println!("Enter your message: ");
+        let mut user_input: String = String::new();
+        match std::io::stdin().read_line(&mut user_input) {
+            Ok(_) => return user_input.trim().to_string(),
+            Err(_) => return "".to_string(),
+        };
+    });
+
+    // This takes forever to load
+    let mut person1 = Person::new(String::from("person1"));
+    let person2 = Person::new(String::from("person2 "));
+
+    while !user_input_thread.is_finished() {
+        println!("loading...");
+        thread::sleep(Duration::from_millis(500));
     }
+    let user_input = user_input_thread.join().unwrap();
+    RSA::encrypt(&mut person1, user_input, &person2);
+    println!("{:?}", RSA::decrypt(&person2));
 }
